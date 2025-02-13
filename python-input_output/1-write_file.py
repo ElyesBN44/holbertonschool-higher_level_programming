5b7d4fc7928fc20efe31bba793925359220ff674
root <root@Elyes.>
2025-02-13 15:34:24 +0100

    aaa

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Module to write a string to a UTF-8 text file."""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a UTF-8 text file and returns the number of characters written."""
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)
