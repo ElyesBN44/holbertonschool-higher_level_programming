4fe7b87b44ba1520265b727d4de6d2cacf4eb627
root <root@Elyes.>
2025-02-13 15:28:55 +0100

    aaa

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,6 +2,7 @@
 
 """Module to read a UTF-8 text file and print its content to stdout."""
 
+
 def read_file(filename=""):
     """Reads a UTF-8 text file and prints its content to stdout."""
     with open(filename, "r", encoding="utf-8") as file:
